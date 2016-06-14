4542e3452e485972590fffee74e1d551bffc3490
David Melvin <dmelvin@fb.com>
2016-06-14 14:17:11 -0700

    Initial. Basic tip calculator should be working but needs check

--- Tipper/ViewController.swift
+++ Tipper/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //allow these labels to be changed within the view, by creating an outlet for it
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true) //dismiss keyboard
+    }
 
+    @IBAction func calculateTip(sender: AnyObject) {
+        let tipPercentages = [0.15, 0.20, 0.25]
+        
+        ///let because I won't change this value later. Isn't it automatically changing though?
+        // What is the exclamation point for?
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex];
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
 }
 
